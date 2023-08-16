14fa7839c6bca69095921044f4771fdf141dd967
AshYoon <rlawodbs7691@naver.com>
2023-08-16 19:04:07 +0900

    BOJ 1436
    완전탐색

--- BOJ.cpp
+++ BOJ.cpp
@@ -1,4 +1,3 @@
-//vscode teset
 #include <iostream>
 #include <vector>
 #include <string>
@@ -6,7 +5,50 @@
 
 using namespace std;
 
+//vscode test
+
 int main()
 {
-    cout << " test " << endl;
+    int n = 0;
+   long long startnumber = 666;
+   int targetnumber ;
+   int cnt = 0;
+   int checkcnt = 0;
+   string temp = "";
+   cin >> targetnumber;
+   for(long long i = startnumber ; i < 9999999; i++)
+   {
+
+    temp = to_string(i);
+    checkcnt = 0;
+    for(auto c : temp)
+    {
+        if(c == '6') checkcnt++;
+        else 
+        checkcnt = 0;
+
+        if( checkcnt == 3 )
+        {
+            cnt++;
+            break;
+        }
+        
+
+    }
+    if(cnt == targetnumber)
+    {
+        cout << i ;
+        break;
+    } 
+    else 
+    {
+        continue;
+    }
+
+
+   }
+
+   return 0;
+
+
 }
