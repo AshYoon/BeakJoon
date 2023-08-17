60377ee1eab21de9cb699fc410d3aafde65bc477
AshYoon <rlawodbs7691@naver.com>
2023-08-17 20:19:59 +0900

    BOJ 2839

--- BOJ.cpp
+++ BOJ.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 //vscode test
-
+int N;
 int main()
 {
     // BOJ 1436
@@ -59,6 +59,21 @@ int main()
     // 가능한 적은수 패키지를 가져간다 , n 킬로를 정확히 운송하는 최소한의 패키지 수 
     // 입력값 n 은 3 <=  N <= 5000
     // n 킬로를 정확이 운송할수없다면 -1 return 
+    	cin >> N;
+	int ans = 0;
+	while (N>=0) {
+		if (N % 5 == 0) {	//가장 큰 수로 나누는게 가장 작은수랑 섞어서 나누는 것보다 유리
+			ans += (N / 5);	//나눈 몫을 더한 것이 정답
+			cout << ans << endl;
+			return 0;
+		}
+		N -= 3;	//3kg을 빼고 
+		ans++;	//가방 하나 늘어남
+	}
+	cout << -1 << endl;
+
+
+
 
     
 
