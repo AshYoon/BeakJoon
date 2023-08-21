8e890b69739588dfd87652139b5c2a02ec58570c
AshYoon <rlawodbs7691@naver.com>
2023-08-21 21:15:09 +0900

    programmers dice game 3 fix

--- BOJ.cpp
+++ BOJ.cpp
@@ -18,77 +18,19 @@ int check(vector<int> v , int n)
 
 int solution(int a, int b, int c, int d) {
     int answer = 0;
-    vector<int> numbers = { a, b ,c ,d};
-    vector <int> test ;
+    vector <int> numbers = { a , b ,c ,d };
+    sort(numbers.begin() , numbers.end());
     
-    sort(numbers.begin(), numbers.end());
-    for(auto n : numbers) cout << n << " ," ;
-    cout << endl;
-    int p =0,q=0,r=0;
-    for(int i = 0 ;i <4;i++)
-    {
-        test.push_back(check(numbers,numbers[i]));
-    }
-    for(auto n : test) cout << n << " ," ;
-    
-    if(test.front() == test.back())
-    {
-        if(test.front() == 4 ) return 1111 * a; // 모든 숫자가 같을때 
-        else if( test.front() == 1 ) return numbers[0]; // 모든숫자가 1개씩 다른 숫자일때 
-        else if( test.front() == 2 )  // 모든숫자가 2개씩 일때 
-        {
-            
-            p = numbers.front();
-            
-            q = numbers.back();
-
-            return (p + q) * abs(p -q);
-        }
-    }
+    if( numbers[0] == numbers[3]) answer = 1111* numbers[0]; // 4개 숫자가 모두같음 
+    else if( numbers[0] == numbers[1] && numbers[2] == numbers[3]) answer = (numbers[0] + numbers[2]) * abs(numbers[0] - numbers[2]); // 3개의 숫자가 같음 
+    else if( numbers[1] == numbers[2] && (numbers[0] == numbers[1] || numbers[2] == numbers[3])) answer = pow(10*numbers[1] + (numbers[0] != numbers[1] ? numbers[0] : numbers[3]) ,2);
+    else if( numbers[0] != numbers[1] && numbers[1] != numbers[2] && numbers[2] != numbers[3] ) answer = numbers[0]; // 모두 다른 숫자일때 
     else
     {
-        vector < pair<int ,int> > check;
-        int cnt1 =0 , cnt2 = 0;
-        cout << endl;
-        for(int i =0;i< 4 ; i++)
-        {
-            check.push_back(make_pair(numbers[i],test[i]));
-            if( test[i] == 1 ) cnt1++;
-            else if( test[i] == 2) cnt2++;
-            
-        }
-        for(auto it : check) cout << it.first << " ," << it.second << endl;
-        if(cnt1 >= 2 && cnt2 >= 2) // 2개의 숫자가 같은 p , q ,r ( q != r)
-        {
-            for(auto it : check) 
-            {
-                
-                if( it.second == 1 && q == 0) 
-                {
-                    q = it.first;
-
-                    continue;
-                }
-                else if( it.second == 1  && q != 0) r = it.first;
-                
-                
-            }
-            
-            return q * r;
-        }
-        else      // 세 주사위가 같고 나머지 다른주사위가 q ( p!= q) 라면 ( 10 x p + q)^2
-        {
-            cout << "test2" << endl;
-            auto it1 = find( test.begin(), test.end() , 3);
-            auto it2 = find( test.begin(), test.end() , 1);
-            p = numbers[*it1-1] , q = numbers[*it2-1]; // 인덱스 접근이라 -1씩 해준다 ? 
-            
-            answer = (10 * p) + q;
-            return pow(answer , 2);
-        }
+        answer = numbers[0] == numbers[1] ? numbers[2] * numbers[3] : (numbers[1] == numbers[2] ? numbers[0] * numbers[3] : numbers[0] * numbers[1]);
     }
-        
-    return 567568;
+    
+    return answer;
 }
 //vscode test
 int N;
