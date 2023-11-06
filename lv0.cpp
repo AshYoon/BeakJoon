cfbf97763406c45f6bbce7e0c3af124b6e96b8bd
AshYoon <rlawodbs7691@naver.com>
2023-11-06 19:06:16 +0900

    Update lv0.cpp

--- lv0.cpp
+++ lv0.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <algorithm>
 using namespace std;
+// 전국 선발 고사 
 //0번부터 n-1 번까지 선발고사 , 등수높은 3명선발 ( 참여가능한 )
 //등수배열 rank , 참여가능여부 목록 attendance , 높은순서대로 a,b,c 
 // 10000 * a + 100 * b + c를 return 
@@ -54,6 +55,8 @@ using namespace std;
 
 // }
 
+
+// 배열 요소 지우기 
 // vector<int> solution(vector<int> arr, vector<int> delete_list) {
 //     vector<int> answer;
     
@@ -69,4 +72,28 @@ using namespace std;
     
     
 //     return answer = arr;
+// }
+
+//달리기 경주 
+// vector<string> solution(vector<string> players, vector<string> callings)
+// {
+//     vector<string> answer;
+//     map<int, string> m1;
+//     map<string, int> m2;
+//     for(int i=0;i<players.size();i++)
+//     {
+//         m1[i] = players[i];
+//         m2[players[i]] = i;
+//     }
+//     for(int i=0;i<callings.size();i++)
+//     {
+//         int cur_idx = m2[callings[i]];//호출 선수 이름, 순위
+//         string change = m1[cur_idx - 1];//앞선 선수 이름, 순위
+//         m1[cur_idx - 1] = callings[i];
+//         m1[cur_idx] = change;
+//         m2[callings[i]] = cur_idx - 1;
+//         m2[change] = cur_idx;
+//     }
+//     for(auto c : m1) answer.push_back(c.second);
+//     return answer;
 // }
